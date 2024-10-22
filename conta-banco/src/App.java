7cc80a456fd230dc7cccd45a7648a0bc717bcdec
dev3.bisa <dev3.bisa@gmail.com>
2024-10-22 18:31:09 -0300

    commit

--- conta-banco/src/App.java
+++ conta-banco/src/App.java
@@ -1,17 +1,9 @@
-import java.util.Arrays;
-import java.util.Comparator;
-import java.util.List;
-import java.util.Optional;
-import java.util.stream.Collectors;
-
 public class App {
 
     public static void main(String[] args) throws Exception {
-        List<Integer> numeros = Arrays.asList(3, 5, 1, 9, 7, 2);
-        Optional<Integer> max = numeros.stream().max(Comparator.naturalOrder());
-        System.out.println(max);
+        ContaTerminal conta = new ContaTerminal();
 
+        conta.obterDadosDoCliente();
+        System.out.println(conta.imprimirDadosDoCliente());
     }
-
-    
 }
--- controle-fluxo/src/Main.java
+++ controle-fluxo/src/Main.java
@@ -1,8 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        for (EstadoBrasileiro e : EstadoBrasileiro.values()){
-            System.out.println(e.getSigla() + " - " + e.getNome());
-        }
-        EstadoBrasileiro eb = EstadoBrasileiro.PIAUI;
+        System.out.println("Hello world!");
     }
 }
